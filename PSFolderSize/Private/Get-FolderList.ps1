5441d32b994830cb0b34a5f64018d88511a40edb
gngrninja <thegngrninja@gmail.com>
2018-06-26 21:58:41 -0700

    Adding option to get folder size of a folder that has no folders in it

--- PSFolderSize/Private/Get-FolderList.ps1
+++ PSFolderSize/Private/Get-FolderList.ps1
@@ -13,7 +13,7 @@ function Get-FolderList {
     
     if ($FolderName -eq 'all' -and $FindExtension) {
 
-        $allFolders = Get-ChildItem $BasePath -Force -Recurse | Where-Object {($_.FullName -notin $OmitFolders) -and ($_.Extension -in $FindExtension)}                
+        $allFolders = Get-ChildItem -Path $BasePath -Force -Recurse | Where-Object {($_.FullName -notin $OmitFolders) -and ($_.Extension -in $FindExtension)}                
 
     } elseif ($folderName -eq 'all') {
 
@@ -21,12 +21,21 @@ function Get-FolderList {
 
     } elseif ($FolderName -ne 'all' -and $FindExtension) {
 
-        $allFolders = Get-ChildItem $BasePath -Force -Recurse | Where-Object {($_.BaseName -like $FolderName) -and ($_.FullName -notin $OmitFolders) -and ($_.Extension -in $FindExtension)}        
+        $allFolders = Get-ChildItem -Path $BasePath -Force -Recurse | Where-Object {($_.BaseName -like $FolderName) -and ($_.FullName -notin $OmitFolders) -and ($_.Extension -in $FindExtension)}        
 
     } else {
 
-        $allFolders = Get-ChildItem $BasePath -Directory -Force | Where-Object {($_.BaseName -like $FolderName) -and ($_.FullName -notin $OmitFolders)}
+        $allFolders = Get-ChildItem -Path $BasePath -Directory -Force | Where-Object {($_.BaseName -like $FolderName) -and ($_.FullName -notin $OmitFolders)}
 
+    }
+
+    #Test for null, return just folder if no subfolders
+    $splitPath = Split-Path $BasePath
+    
+    if ((Test-Path -Path $splitPath -ErrorAction SilentlyContinue) -and ($allFolders -eq $null)) {
+                
+        $allFolders = Get-ChildItem -Path $splitPath -Directory | Where-Object {$_.FullName -eq $BasePath.TrimEnd('/').TrimEnd('\')}
+                
     }
 
     return $allFolders
